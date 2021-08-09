c8de1d6dbe6df7581d9f566a4a3efae562e3ae0b
deepp <tcsdeepp@gmail.com>
2021-08-09 12:23:22 +0000

    my first java project :)

---
+++ src/com/tcs/entity/Gymnasism.java
@@ -0,0 +1,22 @@
+package com.tcs.entity;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Gymnasism {
+	static List<User> users = new ArrayList<User>();
+	public static void main(String[] args) {
+
+		createUsers();
+	}
+	private static void createUsers() {
+		for (int i = 0; i < 10; i++) {
+			User u1 = new User();
+			u1.setName("Deep" + i);
+			u1.setAge((byte)(10 * i));
+			users.add(u1);
+		}
+		System.out.println(users);
+		users.get(0).workout();
+	
+	}
+}
