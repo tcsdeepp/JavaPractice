675ad472626b033e3ebc9e19bbd53a59d25a4ce4
deepp <tcsdeepp@gmail.com>
2021-08-11 10:54:01 +0000

    Threads Basics

--- src/com/tcs/threads/Manager.java
+++ src/com/tcs/threads/Manager.java
@@ -2,10 +2,11 @@ package com.tcs.threads;
 
 public class Manager {
 	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
 		Worker worker1= new Worker();
 		Thread t1 = new Thread(worker1);
 		t1.start();
-		for(int i=0;i<10;i++) {
+	
 			System.out.println(Thread.currentThread().toString()+i);
 		}
 	}
--- src/com/tcs/threads/Worker.java
+++ src/com/tcs/threads/Worker.java
@@ -5,6 +5,11 @@ public class Worker implements Runnable{
 	@Override
 	public void run() {
 		System.out.println(Thread.currentThread());
+		try {
+		Thread.sleep(99999);
+		}catch(InterruptedException e) {
+			//TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-
+}
