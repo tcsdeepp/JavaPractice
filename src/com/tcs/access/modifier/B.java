2b06c3e34a12445837b74b7732ac1d7665ae789a
deepp <tcsdeepp@gmail.com>
2021-08-11 06:15:37 +0000

    access modifier

--- src/com/tcs/access/modifier/B.java
+++ src/com/tcs/access/modifier/B.java
@@ -5,7 +5,7 @@ import com.tcs.access.modifier.A;
 public class B {
 	public static void main(String[] args) {
 		A a = new A();
-		System.out.println(a.age);
+		//System.out.println(a.age);
 	}
 
 }
