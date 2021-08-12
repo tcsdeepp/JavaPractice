d130778a5bd82cb0acc1b37dd6f057932aeecbcc
deepp <tcsdeepp@gmail.com>
2021-08-12 08:56:05 +0000

    Tree Map

--- src/com/tcs/map/HashMapDemo.java
+++ src/com/tcs/map/HashMapDemo.java
@@ -2,19 +2,36 @@ package com.tcs.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
+//student -marks for Math, Biology, physics
 public class HashMapDemo {
 	public static void main(String[] args) {
-		createHashMap();
+		//createHashMap();
+		treeMap(); //sorting is done on Key
 	}
 
+	private static void treeMap() {
+		
+		Map<Integer, Float> mathScores = new TreeMap<Integer, Float>();
+		for (int i = 0; i < 20; i++) {
+			mathScores.put(i, new Float(100 * Math.random()));
+
+		}
+		System.out.println(mathScores);}
+
 	private static void createHashMap() {
-		// TODO Auto-generated method stub
+		
 		Map<String, Float> mathScores = new HashMap<String, Float>();
 		for (int i = 0; i < 20; i++) {
 			mathScores.put("student" + i, new Float(100 * Math.random()));
 
 		}
+		boolean exists = mathScores.containsKey("student1");
+		System.out.println(exists);
+		// mathScores.containsValue(value);
+		mathScores.put("student1", 34F);
+		mathScores.remove("student2");
 		System.out.println(mathScores);
 	}
 }
